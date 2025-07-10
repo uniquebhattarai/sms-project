989939778747be9692627ab2d635b8635ede6913
Unique <uniquebhattarai69@gmail.com>
2025-07-10 14:48:19 +0545

    Logout issue solved

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import logo from '../assets/finalLogo.png';
 import toast from 'react-hot-toast';
 
 export default function Navbar({setIsLoggedIn}) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
