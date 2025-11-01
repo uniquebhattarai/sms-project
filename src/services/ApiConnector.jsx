f80b9409abbb4b52fb3cffb42cade188c8a9586a
Unique <uniquebhattarai69@gmail.com>
2025-11-02 03:18:51 +0545

    api changed for hosting

--- src/services/ApiConnector.jsx
+++ src/services/ApiConnector.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 
 //  export const BASE_URL = "https://prejjj.pythonanywhere.com/api"; 
 
-export const BASE_URL = "http://127.0.0.1:8000/api";
-// export const BASE_URL = "https://api-prejan.mokshya.tech/api";
+// export const BASE_URL = "http://127.0.0.1:8000/api";
+export const BASE_URL = "https://api-prejan.mokshya.tech/api";
 
 
 export const axiosInstance = axios.create({
